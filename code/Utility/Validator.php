c9dadfc73b126a948a6b1b4796539f85399349bd
Marty Eason <dev@zawntech.com>
2016-08-06 01:49:02 -0400

    Implement an error string and getter function

--- code/Utility/Validator.php
+++ code/Utility/Validator.php
@@ -4,6 +4,11 @@ namespace Zawntech\WordPress\Utility;
 class Validator
 {
     /**
+     * @var string
+     */
+    protected static $error;
+
+    /**
      * Verify that an array has all of a set of required keys
      * @param array $args
      * @param array $requiredKeys
@@ -23,10 +28,22 @@ class Validator
         {
             if ( ! in_array( $arg, $requiredKeys ) )
             {
+                // Set error message.
+                static::$error = "Argument key: '$arg' is not supplied.";
+
+                // Return boolean.
                 return false;
             }
         }
 
         return true;
+    }
+
+    /**
+     * @return string
+     */
+    public static function getError()
+    {
+        return static::$error;
     }
 }
