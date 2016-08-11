0e4c0c538d66f245c3a4ea93eb95b4ead7670122
Marty Eason <dev@zawntech.com>
2016-08-10 23:22:08 -0400

    Add global function get_edit_post_url($postId)

--- code/Utility/Functions.php
+++ code/Utility/Functions.php
@@ -28,3 +28,16 @@ if ( ! function_exists('get_no_image_url') )
         return WORDPRESS_HELPERS_URL . 'assets/img/no-image-150.png';
     }
 }
+
+if ( ! function_exists('get_edit_post_url') )
+{
+    /**
+     * Returns an edit post URL for a given post ID.
+     * @param $postId
+     * @return string
+     */
+    function get_edit_post_url($postId)
+    {
+        return admin_url() . 'post.php?post=' . $postId . '&action=edit';
+    }
+}
