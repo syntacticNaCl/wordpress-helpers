5e9346b19108e4dd98a4f7355e16cbd97634978d
Marty Eason <dev@zawntech.com>
2016-08-15 15:46:55 -0400

    Implement a PostTypeModel

---
+++ code/PostTypes/PostTypeModel.php
@@ -0,0 +1,31 @@
+<?php
+namespace PostTypes;
+
+class PostTypeModel
+{
+    public $postId;
+    public $title;
+    public $slug;
+
+    /**
+     * PostTypeModel constructor.
+     * @param $postId
+     */
+    public function __construct($postId)
+    {
+        // Get post.
+        $post = get_post($postId);
+
+        // No post?
+        if ( null === $post ) {
+            return;
+        }
+
+        // Set post ID.
+        $this->postId = $postId;
+
+        // Set name.
+        $this->title = $post->post_title;
+        $this->slug = $post->post_name;
+    }
+}
