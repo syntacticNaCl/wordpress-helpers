6008a94f3c807595096422fa5b210087ead44c0b
Marty Eason <dev@zawntech.com>
2016-09-02 14:45:50 -0400

    Update featured image model

--- code/PostTypes/PostTypeModel.php
+++ code/PostTypes/PostTypeModel.php
@@ -9,12 +9,23 @@ abstract class PostTypeModel
      * @var array
      */
     protected $options = [
-        'loadFeaturedImage' => true
+        'loadFeaturedImage' => true,
+        'loadMeta' => true
     ];
+
+    /**
+     * @var 
+     */
+    protected $metaClass;
 
     public $postId;
     public $title;
     public $slug;
+
+    /**
+     * @var PostMeta
+     */
+    public $meta;
 
     /**
      * @var FeaturedImageModel
@@ -43,9 +54,25 @@ abstract class PostTypeModel
         $this->slug = $post->post_name;
 
         // If the extending models are configured to load featured images.
-        if ( $this->options['loadFeatureImage'] )
+        if ( $this->options['loadFeaturedImage'] )
         {
             $this->featuredImage = new FeaturedImageModel( $postId );
+        }
+
+        // Autoload meta.
+        if ( $this->options['loadMeta'] )
+        {
+            // Verify that a class defintion is provided.
+            if ( ! $this->metaClass )
+            {
+                throw new \Exception('No PostMeta class defined in class.');
+            }
+
+            // Reference class name.
+            $className = $this->metaClass;
+
+            // Instantiate post meta.
+            $this->meta = new $className($postId);
         }
     }   
 }
